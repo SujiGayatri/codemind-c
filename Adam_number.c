ad177ce5ffca5c3092307ae1f4caa7fa824f3826
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2022-12-20 15:36:25 +0530

    #include<stdio.h> int main() {     int n,n1,sq,sq1,r,q,s=0,r1,q1,s1=0,rsq;     scanf("%d",&n);     q=n;     while(q!=0)     {         r=q%10;         q=q/10;         s=s*10+r;     }     n1=s;     sq=n*n;     sq1=n1*n1;     q1=sq1;     while(q1!=0)     {         r1=q1%10;         q1=q1/10;         s1=s1*10+r1;     }     rsq=s1;     if(sq==rsq)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Adam_number.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int n,n1,sq,sq1,r,q,s=0,r1,q1,s1=0,rsq;
+    scanf("%d",&n);
+    q=n;
+    while(q!=0)
+    {
+        r=q%10;
+        q=q/10;
+        s=s*10+r;
+    }
+    n1=s;
+    sq=n*n;
+    sq1=n1*n1;
+    q1=sq1;
+    while(q1!=0)
+    {
+        r1=q1%10;
+        q1=q1/10;
+        s1=s1*10+r1;
+    }
+    rsq=s1;
+    if(sq==rsq)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
