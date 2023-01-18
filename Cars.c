2a4cdfc2daf6b1e541bcdad19ded1cd3c89ca70e
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-01-18 11:53:27 +0530

    #include<stdio.h> int main() {     int n,r,q,m;     scanf("%d",&n);     r=n%4;     q=n/4;     m=q+1;     if(r==0)     {         printf("%d",q);     }     else     {         printf("%d",m);     } }

---
+++ Cars.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,r,q,m;
+    scanf("%d",&n);
+    r=n%4;
+    q=n/4;
+    m=q+1;
+    if(r==0)
+    {
+        printf("%d",q);
+    }
+    else
+    {
+        printf("%d",m);
+    }
+}
