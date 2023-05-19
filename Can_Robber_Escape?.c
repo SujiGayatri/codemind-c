197c89c550970a3dfa35f69f67b3817e6203ff2f
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2023-05-19 15:06:08 +0530

    #include<stdio.h> int main() {     int n,c=0,i,a[100];      scanf("%d",&n);      for (i=0;i<n;i++)     {        scanf("%d",&a[i]);      }     for(i=0;i<n;i++)     {     if(a[i]%2!=0)        c+=1;     }     if (c<=2)     {          printf("YES");     }     else     {          printf("NO");     } }

--- Can_Robber_Escape?.c
+++ Can_Robber_Escape?.c
@@ -1,25 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,c=0,a[100];
-    scanf("%d",&n);
+    int n,c=0,i,a[100];
+     scanf("%d",&n);
+     for (i=0;i<n;i++)
+    {
+       scanf("%d",&a[i]);
+     }
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
-    }
-    for(i=0;i<n;i++)
-    {
-        if(a[i]%2!=0)
-        {
-            c+=1;
-        }
+    if(a[i]%2!=0)
+       c+=1;
     }
     if (c<=2)
     {
-    printf("YES");
+         printf("YES");
     }
     else
     {
-    printf("NO");
+         printf("NO");
     }
 }
